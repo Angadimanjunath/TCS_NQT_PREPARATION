1e66fa4d8d6a0d7b9636c8ed8fb3f0fd346ac307
Angadimanjunath <angadimanjunath@gmail.com>
2026-06-15 21:53:17 +0530

    update

---
+++ Day 9/main.java
@@ -0,0 +1,17 @@
+class Student {
+    String name;
+    int age;
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Manju";
+        s1.age = 21;
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+    }
+}
